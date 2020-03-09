af508c06e7f451412cd1fd02df1fbb16284b1a72
alirn76 <alirn76@yahoo.com>
2020-03-09 17:01:25 +0330

    label form upload delete shod

--- receiver/forms.py
+++ receiver/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from receiver.models import Document
 
 class DocumentForm(forms.ModelForm):
+    document = forms.FileField(label="")
     class Meta:
         model = Document
         fields = ('document', )
